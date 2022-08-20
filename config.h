3088b12318b790d8f3b6ad108f15ca9ca9ce3a0b
Sam <tritoke@protonmail.com>
2022-08-20 06:06:51 +0100

    colours?

--- config.h
+++ config.h
@@ -5,14 +5,14 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrains Mono:size=9",
-	"Noto Color Emoji:size=9", /* colour emoji */
+	"JoyPixels:size=9", /* colour emoji */
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*                fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
