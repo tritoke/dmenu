a9bdc248cad968f13cde8bdb336fc3b57a937f97
Sam Leonard <samuelleonard35@gmail.com>
2024-05-18 20:12:19 +0100

    ✨ Default to berkeley mono

--- config.h
+++ config.h
@@ -4,6 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
+	"Berkeley Mono:size=9",
 	"JetBrains Mono:size=9",
 	"Noto Color Emoji:size=9", /* colour emoji */
 };
